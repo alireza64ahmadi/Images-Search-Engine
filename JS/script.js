9516051c4deed5f06db289fa8fdbad110f7aaa13
alireza <121108490+alireza64ahmadi@users.noreply.github.com>
2024-01-18 02:39:00 +0330

    "Add comment to the js codes"
    "Add comment to the js codes"

--- JS/script.js
+++ JS/script.js
@@ -1,52 +1,54 @@
+// Set access key for Unsplash API
 const accessKey = "UBAZgVD0-Ji-d1C-gq-cKn3HxkjZwJLuE6Hcxb_nwXg";
 
+// Select relevant DOM elements
+const searchForm = document.querySelector("#search-form");
+const searchBox = document.querySelector("#search-box");
+const searchResult = document.querySelector("#search-result");
+const showMoreBtn = document.querySelector("#show-more-btn");
 
-const searchForm = document.querySelector("#search-form")
-const searchBox = document.querySelector("#search-box")
-const searchResult = document.querySelector("#search-result")
-const showMoreBtn = document.querySelector("#show-more-btn")
-
+// Initialize keyword and page variables
 let keyword = "";
 let page = 1;
 
-
+// Asynchronous function to fetch and display images from the Unsplash API
 async function searchImages(){
-    keyword = searchBox.value
+    keyword = searchBox.value;
     const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}&per_page=12`;
 
     const response = await fetch(url);
     const data = await response.json();
 
+    // Clear search results if it's a new search
     if (page === 1) {
-        searchResult.innerHTML = ""
+        searchResult.innerHTML = "";
     }
 
+    // Display fetched images
     const results = data.results;
-
     results.map((result) => {
-        const image = document.createElement("img")
+        const image = document.createElement("img");
         image.src = result.urls.small;
-        const imageLink = document.createElement("a")
+        const imageLink = document.createElement("a");
         imageLink.href = result.links.html;
         imageLink.target = "_blank";
-
         imageLink.appendChild(image);
         searchResult.appendChild(imageLink);
+    });
 
-    })
-
-    showMoreBtn.style.display = "block"
+    // Display 'Show More' button
+    showMoreBtn.style.display = "block";
 }
 
-
+// Event listener for search form submission
 searchForm.addEventListener("submit" , (e) => {
-    e.preventDefault()
+    e.preventDefault();
     page = 1;
-    searchImages()
-})
+    searchImages();
+});
 
-
+// Event listener for 'Show More' button click
 showMoreBtn.addEventListener("click" , () => {
     page++;
     searchImages();
-})
+});
